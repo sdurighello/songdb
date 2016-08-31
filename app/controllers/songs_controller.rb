4deeeb6b7f540ba71fb27a44b7771c153e4e17bd
caisepolente <steve.durighello@gmail.com>
2016-08-31 17:04:32 +0200

    little changes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,7 @@ class SongsController < ApplicationController
 
     respond_to do |format|
       if @song.save
-        format.html { redirect_to @song, notice: 'Song was successfully created.' }
+        # format.html { redirect_to @song.artist, notice: 'Song was successfully created.' }
         format.json { render :show, status: :created, location: @song }
       else
         format.html { render :new }
